f9822872d6b01e31a13ebbad1110f9f67fc3e8f3
DmitriiDudarow <dudarow.d@yandex.ru>
2024-11-01 08:08:25 +0500

    dudarow task 2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,8 @@
 package com.example.task02;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
@@ -9,5 +11,19 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+
+
+        InputStream inputStream = System.in;
+        int prev = inputStream.read();
+        int curr = inputStream.read();
+
+        while (prev != -1){
+            if(prev != 13 || curr != 10){
+                System.out.write(prev);
+            }
+            prev = curr;
+            curr = inputStream.read();
+        }
+        System.out.flush();
     }
 }
